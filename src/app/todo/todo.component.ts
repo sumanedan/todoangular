f7aced7abbca49bb16beb783744d71cf8fa43bee
Suman george <sumanedan@gmail.com>
2020-09-09 23:46:19 +0530

    todolist angular complete

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 dolist:string[]=[];
+tomlist:string[]=[];
 todoval:string;
   todo() 
   {
@@ -17,7 +18,30 @@ todoval:string;
     }else{
       alert("Empty....Enter Value")
     }
+
   }
+  
+  todorem(l){
+    console.log("todo list remove fn called.....")
+    //console.log(l)
+    this.dolist.splice(l,1)
+
+    console.log(this.dolist)
+  
+    
+  }
+  tomadd(m,k){
+  console.log("tomorrow list add fn called")
+  this.tomlist.push(m)
+  console.log(this.tomlist)
+  this.dolist.splice(k,1)
+  }
+  tomrem(v){
+    console.log("tomorrow list remove fn called")
+    this.tomlist.splice(v,1)
+    console.log(this.tomlist)
+  }
+
   constructor() { }
 
   ngOnInit(): void {
